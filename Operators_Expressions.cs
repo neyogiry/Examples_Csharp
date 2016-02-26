72f9cce387afa72f4932970985cf2736f5e0285a
neyogiry <yrigoyen.fernando@gmail.com>
2016-02-26 00:31:34 -0500

    Create Operators_Expressions

---
+++ Operators_Expressions.cs
@@ -0,0 +1,77 @@
+using System;
+
+public class Hello_Neyo{
+	static void Main(string[] args){
+		
+		int x, y, a, b;
+
+		// Assigment operator
+		x = 3;
+		y = 2;
+		a = 1;
+		b = 0;
+
+		// There are many mathematical operators
+
+		// Addittion operator
+		x = 3 + 4;
+
+		// Subtraction operator
+		x = 4 -3;
+
+		// Multiplication operator
+		x = 10 * 5;
+
+		// Division operator
+		x = 10 / 5;
+
+		// There are many operators used to evaluate values
+
+		// Equality operator
+		if (x == y)
+		{			
+		}
+
+		// Greater than operator
+		if (x > y)
+		{			
+		}
+
+		// Less operator
+		if (x < y)
+		{			
+		}
+
+		// Greater or equal to operator
+		if (x > y)
+		{			
+		}
+
+		// Less than or equal to operator
+		if (x > y)
+		{			
+		}
+
+		// There are two "conditional" operators as well that can be used to expand / enhance
+		// an evaluation and they can be conbined together multiple times.
+
+		// Conditional AND operator
+		if ((x > y) && (a > b))
+		{			
+		}
+
+		// Conditional OR operator
+		if ((x > y) || (a > b))
+		{			
+		}
+
+
+		// Also, here's the in-line conditional operator we learned about 
+		// in the previous lesson
+		string message = (x == 1) ? "Positive" : "Negative";
+
+		// Menber acces and Method invocation
+		Console.WriteLine("Hello Neyo!");
+
+	}
+}
